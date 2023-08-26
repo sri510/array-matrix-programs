5af9ebf970a3c29615990dcd58d4770794f7b6e9
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-08-26 22:08:34 +0530

    10.c

--- 10.c
+++ 10.c
@@ -1,22 +1,24 @@
 /*
-Write a C program to read elements in a matrix and check whether matrix is Sparse matrix or not.
-C program for determining sparse matrix. How to check sparse matrix in C. Logic to check sparse matrix in C programming.
+Write a C program to read elements in a matrix and check whether the given matrix is symmetric matrix or not.
+How to check symmetric matrix in C. Logic to check symmetric matrix in C programming.
 Example
 Input
-Input elements in matrix: 
-1 0 3
-0 0 4
-6 0 0
+Input matrix elements: 
+1 2 3
+2 4 5
+3 5 8
 Output
-The given matrix is Sparse matrix
+Given matrix is symmetric matrix.
 */
 #include <stdio.h>
 #define SIZE 3
 
 int main()
 {
-    int A[SIZE][SIZE];
-    int row, col, total=0;
+    int A[SIZE][SIZE];  
+    int B[SIZE][SIZE];  
+
+    int row, col, isSymmetric;
 
     printf("Enter elements in matrix of size 3x3: \n");
     for(row=0; row<SIZE; row++)
@@ -27,24 +29,48 @@ int main()
         }
     }
 
+    
     for(row=0; row<SIZE; row++)
     {
         for(col=0; col<SIZE; col++)
         {
-            if(A[row][col] == 0)
+            B[row][col] = A[col][row];
+        }
+    }
+
+
+    
+    isSymmetric = 1;
+    for(row=0; row<SIZE && isSymmetric; row++)
+    {
+        for(col=0; col<SIZE; col++)
+        {
+            if(A[row][col] != B[row][col])
             {
-                total++;
+                isSymmetric = 0;
+                break;
             }
         }
     }
 
-    if(total >= (row * col)/2)
+    
+    if(isSymmetric == 1)
     {
-        printf("\nThe given matrix is a Sparse matrix.");
+        printf("\nThe given matrix is Symmetric matrix: \n");
+
+        for(row=0; row<SIZE; row++)
+        {
+            for(col=0; col<SIZE; col++)
+            {
+                printf("%d ", A[row][col]);
+            }
+
+            printf("\n");
+        }
     }
     else
     {
-        printf("\nThe given matrix is not Sparse matrix.");
+        printf("\nThe given matrix is not Symmetric matrix.");
     }
 
     return 0;
